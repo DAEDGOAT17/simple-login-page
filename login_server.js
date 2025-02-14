40a17dbf505cdccd231570e16a347ba2f9818d78
DAEDGOAT17 <pankajth2970@gmail.com>
2025-02-14 14:37:33 +0530

    Update login_server.js

--- login_server.js
+++ login_server.js
@@ -44,7 +44,7 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 });
 
-app.post('/',async (req,res)=>{
+app.post('/login',async (req,res)=>{
     const user = new loginDB({
         user_name: req.body.username,
         password: req.body.password
